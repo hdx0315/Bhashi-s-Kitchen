b7406243ad581db4c4585ec1708095bf09068fa2
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-04-01 00:43:10 +0530

    Update Popular.jsx

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -13,11 +13,23 @@ function Popular() {
 
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=9f1b8c98ff7a41c89e3e1a10669795f2&number=5`);
-        
-        const data = await api.json();
 
-        setPopular(data.recipes)
+        const check = localStorage.getItem("popular"); 
+        
+        if (check) {
+            setPopular(JSON.pharse(check))
+        }
+        else{
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=9f1b8c98ff7a41c89e3e1a10669795f2&number=9`);
+            
+            const data = await api.json();
+             
+            localStorage.setItem("popular", JSON.stringify(data.recipes))
+            setPopular(data.recipes)
+        }
+
+
+
 
     }
 
@@ -32,15 +44,15 @@ function Popular() {
                 perPage: 5,
                 arrows: false,
                 pagination: false,
-                drag: 'free',
-                gap: '5rem'
+                drag: "free",
+                gap: '5rem',
             }}>            
                 { popular.map((recipe) => {
                     return(
                         <SplideSlide key={recipe.id}>
                             <Card >
                                 <p>{recipe.title}</p>
-                                <img src={recipe.image} alt="" />
+                                <img src={recipe.image} alt={recipe.title} />
                             </Card>
                         </SplideSlide>
                     );
@@ -59,10 +71,42 @@ const Card = styled.div`
     min-height: 25rem;
     border-radius: 2rem;
     overflow: hidden;
+    position: relative;
+
     
     img{
         border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%
+        object-fit: cover;
+
+    }
+
+    p{
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        width: 100%
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
     
+`;
+
+const Gradient = styled.dv`
+    z-index: 3;
+    40.04
+
 `
+
 export default Popular
