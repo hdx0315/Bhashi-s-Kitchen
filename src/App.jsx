a78b89dfad73e8b6259fb08ae083b68bc6f92675
Hirusha Dinujaya <hirushadinujaya0315@gmail.com>
2024-04-08 20:07:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Nav>
-        <GiKnifeFork/>
         <Logo to={"/Bhashi-s-Kitchen"}>
+        <GiKnifeFork/>
           Bhashi's Kitchen
         </Logo>
       </Nav>
